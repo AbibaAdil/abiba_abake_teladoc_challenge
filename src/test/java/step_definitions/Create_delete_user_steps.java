cdf4b6bfcc95f2e68ff71c0719414773b88809a8
AbibaAbake <94093914+AbibaAbake@users.noreply.github.com>
2021-11-11 23:34:10 -0500

    Update Create_delete_user_steps.java

--- src/test/java/step_definitions/Create_delete_user_steps.java
+++ src/test/java/step_definitions/Create_delete_user_steps.java
@@ -31,14 +31,8 @@ public class Create_delete_user_steps {
 	// add user steps starts
 	@When("I add new user with following details")
 	public void i_add_new_user_with_following_details(DataTable dataTable) {
-	    // Write code here that turns the phrase above into concrete actions
-	    // For automatic transformation, change DataTable to one of
-	    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-	    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-	    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-	    // For other transformations you can register a DataTableType.
 		
-		hp.addUserBtn.click();
+	    hp.addUserBtn.click();
 	    bu.waitUntilElementVisible(hp.firstNamefild);
 	    
 	    //converting dataTable into a list
